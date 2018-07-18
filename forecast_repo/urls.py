9d047a703ecce1b135c51cf38ffed48d4fef893c
matthewcornell <matt@matthewcornell.org>
2018-07-18 10:46:50 -0400

    debugging django_rq.urls

--- forecast_repo/urls.py
+++ forecast_repo/urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
+from django.contrib import admin
 
 
 urlpatterns = [
     url(r'', include('forecast_app.urls')),
+    url(r'^admin/', admin.site.urls),
     url(r'^django-rq/', include('django_rq.urls')),
 ]
 
