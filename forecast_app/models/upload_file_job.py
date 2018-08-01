b0e5199cca35d8dc8b41f189860ee95c3db5b6ca
matthewcornell <matt@matthewcornell.org>
2018-08-01 13:15:28 -0400

    WIP S3 working pretty much abstracted!

--- forecast_app/models/upload_file_job.py
+++ forecast_app/models/upload_file_job.py
@@ -1,7 +1,6 @@
 import logging
 import tempfile
-import time
-from io import SEEK_END
+from contextlib import contextmanager
 
 import boto3
 from django.db import models
@@ -9,6 +8,7 @@ from django.db.models import BooleanField
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from django.shortcuts import get_object_or_404
+from jsonfield import JSONField
 
 from forecast_app.models.counter import basic_str
 
@@ -22,9 +22,6 @@ class UploadFileJob(models.Model):
     """
     Holds information about user file uploads. Accessed by worker jobs when processing those files.
     """
-    #
-    # general task-related fields
-    #
 
     PENDING = 0
     S3_FILE_UPLOADED = 1
@@ -41,6 +38,8 @@ class UploadFileJob(models.Model):
     )
     status = models.IntegerField(default=PENDING, choices=STATUS_CHOICES)
 
+    # user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)  # user who submitted
+
     created_at = models.DateTimeField(auto_now_add=True)  # when this instance was created. basically the submit date
 
     updated_at = models.DateTimeField(auto_now=True)  # time of last save(). basically last time status changed
@@ -51,37 +50,16 @@ class UploadFileJob(models.Model):
 
     filename = models.CharField(max_length=200)  # original name of the uploaded file
 
+    # app-specific data passed to the UploadFileJob from the request. ex: 'model_pk':
+    input_json = JSONField(null=True, blank=True)
 
-    #
-    # user-related fields
-    #
-
-    # user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)  # user who submitted
-
-
-    #
-    # app-specific fields
-    #
-
-    # model_pk = models.IntegerField()  # placeholder for: forecast_model = models.ForeignKey(ForecastModel, ...)
-
-    # timezero_date = models.DateField(help_text="A date that a target is relative to.")
-
-    # data_version_date = models.DateField(
-    #     null=True, blank=True,
-    #     help_text="The optional database date at which models should work with for the timezero_date.")  # nullable
-
-    # new_forecast_pk (FK, NULL)  # aka the result
-
-
-    # >> todo xx abstract this class
-    # class Meta:
-    #     abstract = True
+    # app-specific results from a successful completion of the upload. ex: 'forecast_pk':
+    output_json = JSONField(null=True, blank=True)
 
 
     def __repr__(self):
         return str((self.pk, self.created_at, self.updated_at, self.filename, self.status_as_str(),
-                    self.is_failed, self.failure_message))
+                    self.is_failed, self.failure_message, self.input_json, self.output_json))
 
 
     def __str__(self):  # todo
@@ -101,10 +79,10 @@ class UploadFileJob(models.Model):
 
 
     #
-    # service-specific keys/ids
+    # S3 and RQ service-specific keys/ids
     #
-    # todo should UploadFileJob know about RQ and S3? maybe some kind of adapter to separate concerns
-    #
+
+    # todo should UploadFileJob know about RQ and S3 at all? maybe some kind of adapter to separate concerns
 
     def s3_key(self):
         """
@@ -137,47 +115,61 @@ class UploadFileJob(models.Model):
             logger.debug("delete_s3_object(): Failed: {}, {}".format(exc, self))
 
 
-    @classmethod
-    def process_uploaded_file(cls, upload_file_job_pk):
-        """
-        enqueue() helper function. processes the passed UploadFileJob's pk: downloads the corresponding S3 file, loads
-        it into the database via load_forecast(), deletes the S3 file, and sets the job's status.
-        """
-        upload_file_job = get_object_or_404(UploadFileJob, pk=upload_file_job_pk)
-        logger.debug("process_uploaded_file(): Started. upload_file_job={}".format(upload_file_job))
-        with tempfile.TemporaryFile() as temp_fp:
-            try:
-                logger.debug("process_uploaded_file(): Downloading from S3: {}, {}. upload_file_job={}"
-                             .format(S3_UPLOAD_BUCKET_NAME, upload_file_job.s3_key(), upload_file_job))
-                s3 = boto3.client('s3')  # using client here instead of higher-level resource b/c want to save to a fp
-                s3.download_fileobj(S3_UPLOAD_BUCKET_NAME, upload_file_job.s3_key(), temp_fp)
-                upload_file_job.status = UploadFileJob.S3_FILE_DOWNLOADED
-                upload_file_job.save()
+#
+# the context manager for use by django_rq.enqueue() calls by views._upload_file()
+#
 
-                # >> todo xx abstract this, e.g., upload_file_job.process_s3_file()
-                logger.debug("process_uploaded_file(): Loading forecast. upload_file_job={}".format(upload_file_job))
-                time.sleep(5)  # simulates a long-running operation
-                # file_size = temp_fp.seek(0, SEEK_END)
-                # temp_fp.seek(0)
-                # lines = temp_fp.readlines()
-                # new_forecast = load_forecast()  # todo the actual work :-)
-                # upload_file_job.new_forecast_pk = new_forecast.pk
-                # upload_file_job.save()
+@contextmanager
+def upload_file_job_s3_file(upload_file_job_pk):
+    """
+    A context manager for use by django_rq.enqueue() calls by views._upload_file().
 
-                upload_file_job.status = UploadFileJob.SUCCESS  # yay!
-                upload_file_job.save()
-                logger.debug("process_uploaded_file(): Done. upload_file_job={}".format(upload_file_job))
-            except Exception as exc:
-                failure_message = "process_uploaded_file(): FAILED_PROCESS_FILE: Error: {}. upload_file_job={}" \
-                    .format(exc, upload_file_job)
-                upload_file_job.is_failed = True
-                upload_file_job.failure_message = failure_message
-                upload_file_job.save()
-                logger.debug(failure_message)
-            finally:
-                upload_file_job.delete_s3_object()  # NB: in current thread
+    Does the following setup:
+    - get the UploadFileJob for upload_file_job_pk
+    - download the corresponding S3 object/file data into a temporary file, setting the UploadFileJob's status to
+      S3_FILE_DOWNLOADED
+    - pass the temporary file's fp to this context's caller
+    - set the UploadFileJob's status to SUCCESS
 
+    Does this cleanup:
+    - delete the S3 object, regardless of success or failure
+
+    :param upload_file_job_pk: PK of the corresponding UploadFileJob instance
+    """
+    # __enter__()
+    upload_file_job = get_object_or_404(UploadFileJob, pk=upload_file_job_pk)
+    logger.debug("upload_file_job_s3_file(): Started. upload_file_job={}".format(upload_file_job))
+    with tempfile.TemporaryFile() as s3_file_fp:
+        try:
+            logger.debug("upload_file_job_s3_file(): Downloading from S3: {}, {}. upload_file_job={}"
+                         .format(S3_UPLOAD_BUCKET_NAME, upload_file_job.s3_key(), upload_file_job))
+            s3 = boto3.client('s3')  # using client here instead of higher-level resource b/c want to save to a fp
+            s3.download_fileobj(S3_UPLOAD_BUCKET_NAME, upload_file_job.s3_key(), s3_file_fp)
+            upload_file_job.status = UploadFileJob.S3_FILE_DOWNLOADED
+            upload_file_job.save()
+
+            # make the context call
+            yield upload_file_job, s3_file_fp
+
+            # __exit__()
+            upload_file_job.status = UploadFileJob.SUCCESS  # yay!
+            upload_file_job.save()
+            logger.debug("upload_file_job_s3_file(): Done. upload_file_job={}".format(upload_file_job))
+        except Exception as exc:
+            failure_message = "upload_file_job_s3_file(): FAILED_PROCESS_FILE: Error: {}. upload_file_job={}" \
+                .format(exc, upload_file_job)
+            upload_file_job.is_failed = True
+            upload_file_job.failure_message = failure_message
+            upload_file_job.save()
+            logger.debug(failure_message)
+        finally:
+            upload_file_job.delete_s3_object()  # NB: in current thread
+
+
+#
+# set up a signal to try to delete an UploadFileJob's S3 object before deleting the UploadFileJob
+#
 
 @receiver(pre_delete, sender=UploadFileJob)
 def delete_s3_obj_for_upload_file_job(sender, instance, using, **kwargs):
-    instance.delete_s3_object()  # try to delete the corresponding S3 object
+    instance.delete_s3_object()
--- forecast_app/views.py
+++ forecast_app/views.py
@@ -1,4 +1,6 @@
 import logging
+import time
+from io import SEEK_END
 
 import boto3
 import django_rq
@@ -6,7 +8,7 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 
 from forecast_app.models import Counter, UploadFileJob
-from forecast_app.models.upload_file_job import S3_UPLOAD_BUCKET_NAME
+from forecast_app.models.upload_file_job import S3_UPLOAD_BUCKET_NAME, upload_file_job_s3_file
 
 
 logger = logging.getLogger(__name__)
@@ -16,6 +18,7 @@ def index(request):
     count, last_update = Counter.get_count_and_last_update()
     queue = django_rq.get_queue()  # name='default'
     conn = django_rq.get_connection()  # name='default'
+    # todo xx maybe show if queue is busy?
     return render(request,
                   'index.html',
                   context={'count': count,
@@ -101,24 +104,23 @@ def delete_file_jobs(request):
     return redirect('index')
 
 
-def upload_file(request):  # >> todo xx pass concrete class, such as upload_forecast_file()
-    def create_upload_file_job_fcn(request, data_file):
-        return UploadFileJob.objects.create(filename=data_file.name)  # status = PENDING
+def upload_file(request):  # no-op implementation for testing
+    return _upload_file(request, input_json_for_request__noop, process_upload_file_job__noop)
 
 
-    return _upload_file(request, create_upload_file_job_fcn)
-
-
-def _upload_file(request, create_upload_file_job_fcn):
+def _upload_file(request, input_json_for_request_fcn, process_upload_file_job_fcn):
     """
-    Accepts a file uploaded to this app by the user and then saves it in an S3 bucket.
-    The S3 object key used for the uploaded file is a UploadFileJob's pk that is created to represent this job.
-    The RQ Job.id used is also the UploadFileJob's pk.
-
-    todo use chunks? for chunk in data_file.chunks(): print(chunk)
-
-    :param create_upload_file_job_fcn: a function of two args (request, data_file) that returns a new UploadFileJob
-        subclass instance that's initialized according the the args. data_file is an UploadedFile instance
+    Accepts a file uploaded to this app by the user, saves it in an S3 bucket, then enqueues process_upload_file_job_fcn
+    to process the file by an RQ worker.
+    :param input_json_for_request_fcn: a function of one arg (request) that returns a dict used to initialize the new
+        UploadFileJob's input_json
+    :param process_upload_file_job_fcn: a function of one arg (upload_file_job_pk) that is passed to
+        django_rq.enqueue(). NB: It MUST use this wrapper in order to work have access to the file that was uploaded to
+        S3:
+            with upload_file_job_s3_file() as s3_file_fp: ...
+        NB: If it needs to save upload_file_job.output_json, make sure to call save(), e.g.,
+            upload_file_job.output_json = {'forecast_pk': new_forecast.pk}
+            upload_file_job.save()
     """
     if 'data_file' not in request.FILES:  # user submitted without specifying a file to upload
         save_message_and_log_debug(request, "upload_file(): No file selected to upload.", is_failure=True)
@@ -135,10 +137,13 @@ def _upload_file(request, create_upload_file_job_fcn):
 
     # create the UploadFileJob
     try:
-        upload_file_job = create_upload_file_job_fcn(request, data_file)  # this function's only concrete-specific line
-        save_message_and_log_debug(request, "upload_file(): Created the UploadFileJob: {}".format(upload_file_job))
+        upload_file_job = UploadFileJob.objects.create(filename=data_file.name)  # status = PENDING
+        upload_file_job.input_json = input_json_for_request_fcn(request)
+        upload_file_job.save()
+        save_message_and_log_debug(request, "upload_forecast_file(): 1/3 Created the UploadFileJob: {}"
+                                   .format(upload_file_job))
     except Exception as exc:
-        save_message_and_log_debug(request, "upload_file(): Error creating the UploadFileJob: {}".format(exc),
+        save_message_and_log_debug(request, "upload_forecast_file(): Error creating the UploadFileJob: {}".format(exc),
                                    is_failure=True)
         return redirect('index')
 
@@ -146,11 +151,11 @@ def _upload_file(request, create_upload_file_job_fcn):
     try:
         s3 = boto3.resource('s3')
         bucket = s3.Bucket(S3_UPLOAD_BUCKET_NAME)
+        # todo use chunks? for chunk in data_file.chunks(): print(chunk):
         bucket.put_object(Key=upload_file_job.s3_key(), Body=data_file)
-
         upload_file_job.status = UploadFileJob.S3_FILE_UPLOADED
         upload_file_job.save()
-        save_message_and_log_debug(request, "upload_file(): Uploaded the file to S3: {}, {}. upload_file_job={}"
+        save_message_and_log_debug(request, "upload_file(): 2/3 Uploaded the file to S3: {}, {}. upload_file_job={}"
                                    .format(S3_UPLOAD_BUCKET_NAME, upload_file_job.s3_key(), upload_file_job))
     except Exception as exc:
         failure_message = "upload_file(): FAILED_S3_FILE_UPLOAD: Error uploading file to S3: {}. upload_file_job={}" \
@@ -163,11 +168,11 @@ def _upload_file(request, create_upload_file_job_fcn):
 
     # enqueue a worker
     try:
-        rq_job = django_rq.enqueue(UploadFileJob.process_uploaded_file, upload_file_job.pk,
+        rq_job = django_rq.enqueue(process_upload_file_job_fcn, upload_file_job.pk,
                                    job_id=upload_file_job.rq_job_id())  # name="default"
         upload_file_job.status = UploadFileJob.QUEUED
         upload_file_job.save()
-        save_message_and_log_debug(request, "upload_file(): Enqueued the job: {}. upload_file_job={}"
+        save_message_and_log_debug(request, "upload_file(): 3/3 Enqueued the job: {}. upload_file_job={}"
                                    .format(rq_job, upload_file_job))
     except Exception as exc:
         failure_message = "upload_file(): FAILED_ENQUEUE: Error enqueuing the job: {}. upload_file_job={}" \
@@ -181,3 +186,48 @@ def _upload_file(request, create_upload_file_job_fcn):
 
     logger.debug("upload_file(): Done")
     return redirect('index')
+
+
+#
+# no-op RQ enqueue() helper functions for testing
+#
+
+def input_json_for_request__noop(request):
+    logger.debug("input_json_for_request__noop(): request={}".format(request))
+    return None  # no input_json
+
+
+def process_upload_file_job__noop(upload_file_job_pk):
+    logger.debug("process_upload_file_job__noop(): Loading forecast. upload_file_job_pk={}".format(upload_file_job_pk))
+    with upload_file_job_s3_file(upload_file_job_pk) as (upload_file_job, s3_file_fp):
+        # show that we can access the file's data
+        file_size = s3_file_fp.seek(0, SEEK_END)
+        s3_file_fp.seek(0)
+        lines = s3_file_fp.readlines()
+        logger.debug("process_upload_file_job__noop(): upload_file_job={}.\n\t-> from s3_file_fp: {}, {}, {}"
+                     .format(upload_file_job, file_size, len(lines), repr(lines[0])))
+
+        # simulate a long-running operation
+        time.sleep(5)
+
+
+#
+# app-specific RQ enqueue() helper functions: ForecastModel.load_forecast()
+#
+
+# def input_json_for_request__forecast(request):
+#     # >> todo extract and save app-specific inputs, e.g.,
+#     return {'model_pk': request['model_pk'],
+#             'timezero_date': request['timezero_date'],
+#             'data_version_date': request['data_version_date']}
+
+
+# def process_upload_file_job__forecast(upload_file_job_pk):
+#     upload_file_job = get_object_or_404(UploadFileJob, pk=upload_file_job_pk)
+#     with upload_file_job_s3_file(upload_file_job_pk) as s3_file_fp:
+#         forecast_model_pk = upload_file_job.input_json['model_pk']  # todo 'timezero_date', 'data_version_date', etc.
+#         forecast_model = get_object_or_404(ForecastModel, pk=forecast_model_pk)
+#         # todo csv_file_path, time_zero, file_name=None, validation_template=None, forecast_bin_map=None:
+#         new_forecast = forecast_model.load_forecast(s3_file_fp, ...)
+#         upload_file_job.output_json = {'forecast_pk': new_forecast.pk}
+#         upload_file_job.save()
